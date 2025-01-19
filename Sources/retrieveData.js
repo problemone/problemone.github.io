497005a3809c09e487726709144a2f15f4e127b0
problemone <nickv6211@gmail.com>
2025-01-19 17:33:20 -0600

    Colors adjusted

--- Sources/retrieveData.js
+++ Sources/retrieveData.js
@@ -1,7 +1,7 @@
 let projectData = null;
 let currProjectNum = 1;
 const bodyElement = document.querySelector("body");
-const gameBackColor = rgb(76,76,76);
+const backgroundMinColor = [85, 83, 76];
 
 function retProjectInfo(projectNum){
     let position = null;
@@ -40,9 +40,8 @@ function initializeWebsite(){
         }
 
         let percScroll = window.scrollY/window.innerHeight;
-        let minColor = 76;
         let colorScale = 170;
-        bodyElement.style.backgroundColor = rgb(Math.min(percScroll * colorScale + minColor, minColor + colorScale), Math.min(percScroll * colorScale + minColor, minColor + colorScale), Math.min(percScroll * colorScale + minColor, minColor + colorScale));
+        bodyElement.style.backgroundColor = "rgb(" + Math.min(percScroll * colorScale + backgroundMinColor[0], backgroundMinColor[0] + colorScale) + "," + Math.min(percScroll * colorScale + backgroundMinColor[1], backgroundMinColor[1] + colorScale) + "," +  Math.min(percScroll * colorScale + backgroundMinColor[2], backgroundMinColor[2] + colorScale) + ")";
     });
 
     // Retrieve s3 bucket file with project info
