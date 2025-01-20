1f02312ea937caec9eef1a5d3c717e463faaa996
problemone <nickv6211@gmail.com>
2025-01-19 18:47:29 -0600

    Update

--- Sources/retrieveData.js
+++ Sources/retrieveData.js
@@ -2,6 +2,9 @@ let projectData = null;
 let currProjectNum = 1;
 const bodyElement = document.querySelector("body");
 const backgroundMinColor = [85, 83, 76];
+const colorScale = 170;
+let percScroll = 0;
+let targetColor = backgroundMinColor;
 
 function retProjectInfo(projectNum){
     let position = null;
@@ -11,14 +14,12 @@ function retProjectInfo(projectNum){
     company = specificProject.split(',')[1];
     position = specificProject.split(',')[0];
     
-    console.log("retString: " + position + ";" + company);
     return position + ";" + company;
 }
 
 function selectProject(projectNum){
     currProjectNum = projectNum;
     document.querySelector("#content").scrollIntoView({behavior: "smooth", block: "start", inline: "center"});
-    console.log("projectNum: " + projectNum);
 }
 
 function scrollToTop(){
@@ -28,20 +29,32 @@ function scrollToTop(){
     });
 }
 
+function setBackgroundColor(){
+    targetColor = [percScroll * colorScale + backgroundMinColor[0], percScroll * colorScale + backgroundMinColor[1], percScroll * colorScale + backgroundMinColor[2]]
+    bodyElement.style.backgroundColor = "rgb(" + targetColor[0] + "," + targetColor[1] + "," + targetColor[2] + ")";
+}
+
 function initializeWebsite(){
+    // Set window to top
+    window.scrollTo(0, 0);
+
     // Hide scrollbar if window is at the top
     window.addEventListener('scroll', () => {
         if (window.scrollY == 0) {
             document.querySelector(".Btn").style.visibility = "hidden";
             bodyElement.style.overflowY = "hidden";
+            if(percScroll == 1){
+                percScroll = 0;
+                setBackgroundColor()
+            }
         } else {
             document.querySelector(".Btn").style.visibility = "visible";
             bodyElement.style.overflowY = "visible";
+            if(percScroll == 0){
+                percScroll = 1;
+                setBackgroundColor()
+            }
         }
-
-        let percScroll = window.scrollY/window.innerHeight;
-        let colorScale = 170;
-        bodyElement.style.backgroundColor = "rgb(" + Math.min(percScroll * colorScale + backgroundMinColor[0], backgroundMinColor[0] + colorScale) + "," + Math.min(percScroll * colorScale + backgroundMinColor[1], backgroundMinColor[1] + colorScale) + "," +  Math.min(percScroll * colorScale + backgroundMinColor[2], backgroundMinColor[2] + colorScale) + ")";
     });
 
     // Retrieve s3 bucket file with project info
