28f9495eb405054f4fe48ad611a83c34e759887c
problemone <nickv6211@gmail.com>
2025-01-29 01:23:26 -0600

    Color Fixed

--- Sources/retrieveData.js
+++ Sources/retrieveData.js
@@ -1,12 +1,5 @@
 let projectData = null;
 let currProjectNum = 1;
-const bodyElement = document.querySelector("body");
-const backgroundElement = document.querySelector(".background");
-const backgroundTravelling = document.querySelector(".backgroundTravelling");
-const backgroundMinColor = [85, 83, 76];
-const colorScale = 120;
-let percScroll = 0;
-let targetColor = backgroundMinColor;
 
 function retProjectInfo(projectNum){
     let position = null;
@@ -24,44 +17,7 @@ function selectProject(projectNum){
     document.querySelector("#content").scrollIntoView({behavior: "smooth", block: "start", inline: "center"});
 }
 
-function scrollToTop(){
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-    });
-}
-
 function initializeWebsite(){
-    // Set window to top
-    window.scrollTo(0, 0);
-
-    // Hide scrollbar if window is at the top
-    window.addEventListener('scroll', () => {
-        percScroll = Math.min(window.scrollY/window.innerHeight, 1);
-        if (window.scrollY == 0) {
-            document.querySelector(".Btn").style.visibility = "hidden";
-            bodyElement.style.overflowY = "hidden";
-        } else {
-            document.querySelector(".Btn").style.visibility = "visible";
-            bodyElement.style.overflowY = "visible";
-        }
-        
-        let fadeInElements = document.getElementsByClassName('fadeIn');
-        let relScreenPos = 0;
-
-        for(let i = 0; i < fadeInElements.length; i++){
-            let elementBoundingRect = fadeInElements[i].getBoundingClientRect();
-            relScreenPos = 1-Math.max(0, Math.min((document.documentElement.scrollTop + elementBoundingRect.y - window.scrollY - window.innerHeight * 0.5)/(window.innerHeight * 0.5), 1));
-            console.log("Opacity " + i + ": " + relScreenPos);
-
-            fadeInElements[i].style.opacity = relScreenPos;
-        }
-
-        targetColor = [percScroll * colorScale + backgroundMinColor[0], percScroll * colorScale + backgroundMinColor[1], percScroll * colorScale + backgroundMinColor[2]]
-        backgroundElement.style.backgroundColor = "rgb(" + targetColor[0] + "," + targetColor[1] + "," + targetColor[2] + ")";
-        backgroundTravelling.style.backgroundColor = "rgb(" + targetColor[0] + "," + targetColor[1] + "," + targetColor[2] + ")";
-    });
-
     // Retrieve s3 bucket file with project info
     const config = {
         "apiEndpoint": "https://sdr9a8oax0.execute-api.us-east-2.amazonaws.com/dev/nicholasverzicwebsiteprojects/websiteProjects.csv"
@@ -80,6 +36,9 @@ function initializeWebsite(){
     } catch(e){
         console.log("Retrieval Error: " + e);
     }
+
+    // Set window to top
+    window.scrollTo(0, 0);
 }
 
 initializeWebsite();
---
+++ Sources/setBehavior.js
@@ -0,0 +1,55 @@
+const bodyElement = document.querySelector("body");
+const backgroundElement = document.querySelector(".background");
+const backgroundTravelling = document.querySelector(".backgroundTravelling");
+const backgroundMinColor = [85, 83, 76];
+const colorScale = 120;
+let percScroll = 0;
+let targetColor = backgroundMinColor;
+
+function scrollToTop(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+}
+
+function setBehavior(){
+    // Set contents border
+    let canvasElementRect = document.querySelector("canvas").getBoundingClientRect();
+    let contentElement = document.querySelector("#content");
+    console.log("ContentElement: " + contentElement);
+    contentElement.style.marginLeft = canvasElementRect.x + "px";
+    contentElement.style.marginRight = canvasElementRect.x + "px";
+    console.log("margins: " + canvasElementRect.x + "px");
+    contentElement.style.marginTop = "10vh";
+    console.log("marginsTop: " + "10vh");
+    
+    // Hide scrollbar if window is at the top
+    window.addEventListener('scroll', () => {
+        percScroll = Math.min(window.scrollY/window.innerHeight, 1);
+        if (window.scrollY == 0) {
+            document.querySelector(".Btn").style.visibility = "hidden";
+            bodyElement.style.overflowY = "hidden";
+        } else {
+            document.querySelector(".Btn").style.visibility = "visible";
+            bodyElement.style.overflowY = "visible";
+        }
+        
+        let fadeInElements = document.getElementsByClassName('fadeIn');
+        let relScreenPos = 0;
+
+        for(let i = 0; i < fadeInElements.length; i++){
+            let elementBoundingRect = fadeInElements[i].getBoundingClientRect();
+            let scale = 0.4;
+            relScreenPos = 1-Math.max(0, Math.min((document.documentElement.scrollTop + elementBoundingRect.y - window.scrollY - window.innerHeight * (1-scale))/(window.innerHeight * scale), 1));
+
+            fadeInElements[i].style.opacity = relScreenPos;
+        }
+
+        targetColor = [percScroll * colorScale + backgroundMinColor[0], percScroll * colorScale + backgroundMinColor[1], percScroll * colorScale + backgroundMinColor[2]]
+        backgroundElement.style.backgroundColor = "rgb(" + targetColor[0] + "," + targetColor[1] + "," + targetColor[2] + ")";
+        backgroundTravelling.style.backgroundColor = "rgb(" + targetColor[0] + "," + targetColor[1] + "," + targetColor[2] + ")";
+    });
+}
+
+setBehavior();
